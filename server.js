bc29d5039fcda3f75980bfc2484ce717d99d3380
Astha47 <anasfathurrahman.edu@gmail.com>
2023-06-19 11:19:31 +0700

    cors setting

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended: false}))
 // OAUTH API CORS
 app.use(
     cors({
-        origin: "http//localhost:3000",
+        origin: 'http://localhost:3000'
     })
 )
 
