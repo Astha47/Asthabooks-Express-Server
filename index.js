5302f31e316a58eed160a6121557ecee7e737eb0
Anas Fathurrahman <anasfathurrahman.edu@gmail.com>
2023-10-10 03:21:52 +0700

    update link

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const nodeMailer = require('nodemailer')
 // Mail format
 const html = (username, token) => {
 
-    const link_verification = 'https://asthabooks-next.vercel.app/auth/confirmation/'+username+'/'+token
+    const link_verification = 'https://asthabooks-next.vercel.app/auth/confirmation/gate/'+username+'/'+token
     // const link_verification = 'http://localhost:8000/account/verify/'+username+'/'+token;
 
     return `
